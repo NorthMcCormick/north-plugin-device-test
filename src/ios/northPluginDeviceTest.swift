9fb071fd54e1d36217c967c2bebeaa38f1eb5d03
North McCormick <northmccormick@icloud.com>
2016-05-28 20:33:12 -0700

    +
    + Animations
    + Left/Right Anchor

--- src/ios/northPluginDeviceTest.swift
+++ src/ios/northPluginDeviceTest.swift
@@ -1,4 +1,9 @@
 @objc(northPluginDeviceTest) class northPluginDeviceTest : CDVPlugin {
+    var width: CGFloat = 414.0
+    var height: CGFloat = 736.0
+    var anchorOffset: CGFloat = 0.0
+    let animateDuration = 0.35
+    
 	func setDevice(command: CDVInvokedUrlCommand) {
 		var pluginResult = CDVPluginResult(
 			status: CDVCommandStatus_ERROR
@@ -12,75 +17,83 @@
             
             switch deviceType {
                 case "iPhone4s":
-                    let width: CGFloat = 320.0
-                    let height: CGFloat = 480.0
-                    
-                    self.viewController.view.frame = CGRectMake(UIScreen.mainScreen().bounds.width-width, UIScreen.mainScreen().bounds.height-height, width, height);
+                    self.self.width = 320.0
+                    height = 480.0
                 break;
                 case "iPhoneSE":
-                    
-                    let width: CGFloat = 320.0
-                    let height: CGFloat = 568.0
-                    
-                    self.viewController.view.frame = CGRectMake(UIScreen.mainScreen().bounds.width-width, UIScreen.mainScreen().bounds.height-height, width, height);
+                    self.width = 320.0
+                    self.height = 568.0
                 break;
                 case "iPhone6":
-                    
-                    let width: CGFloat = 375.0
-                    let height: CGFloat = 667.0
-                    
-                    self.viewController.view.frame = CGRectMake(UIScreen.mainScreen().bounds.width-width, UIScreen.mainScreen().bounds.height-height, width, height);
+                    self.width = 375.0
+                    self.height = 667.0
                 break;
                 case "iPhone6Plus":
-                    let width: CGFloat = 414.0
-                    let height: CGFloat = 736.0
-                    
-                    self.viewController.view.frame = CGRectMake(UIScreen.mainScreen().bounds.width-width, UIScreen.mainScreen().bounds.height-height, width, height);
+                    self.width = 414.0
+                    self.height = 736.0
                 break;
                 default:
-                
+                    self.width = 414.0
+                    self.height = 736.0
                 break;
             }
-
-			/* UIAlertController is iOS 8 or newer only. */
-			/*let toastController: UIAlertController = 
-				UIAlertController(
-					title: "", 
-					message: msg, 
-					preferredStyle: .Alert
-				)
-
-			self.viewController?.presentViewController(
-				toastController, 
-				animated: true, 
-				completion: nil
-			)
-
-			let duration = Double(NSEC_PER_SEC) * 3.0
-			
-			dispatch_after(
-				dispatch_time(
-					DISPATCH_TIME_NOW, 
-					Int64(duration)
-				), 
-				dispatch_get_main_queue(), 
-				{ 
-					toastController.dismissViewControllerAnimated(
-						true, 
-						completion: nil
-					)
-				}
-			)
-
-			pluginResult = CDVPluginResult(
-				status: CDVCommandStatus_OK,
-				messageAsString: msg
-			)*/
+            
+            UIView.animateWithDuration(animateDuration, delay: 0, options: .CurveEaseInOut, animations: {
+                var viewFrame = self.viewController.view.frame
+                viewFrame = CGRectMake(self.anchorOffset, UIScreen.mainScreen().bounds.height - self.height, self.width, self.height)
+                
+                self.viewController.view.frame = viewFrame
+                }, completion: { finished in
+                    debugPrint("Finished");
+            })
+            
+            pluginResult = CDVPluginResult(
+                status: CDVCommandStatus_OK
+            )
 		}
+        
 
 		self.commandDelegate!.sendPluginResult(
 			pluginResult, 
 			callbackId: command.callbackId
 		)
 	}
+    
+    func anchor(command: CDVInvokedUrlCommand) {
+        var pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_ERROR
+        )
+        
+        let side = command.arguments[0] as? String ?? ""
+        
+        switch side {
+        case "left":
+            self.anchorOffset = 0.0
+            break;
+        case "right":
+            self.anchorOffset = UIScreen.mainScreen().bounds.width - self.width
+            break;
+        default:
+            
+            break;
+        }
+        
+        UIView.animateWithDuration(animateDuration, delay: 0, options: .CurveEaseInOut, animations: {
+            var viewFrame = self.viewController.view.frame
+            viewFrame = CGRectMake(self.anchorOffset, UIScreen.mainScreen().bounds.height - self.height, self.width, self.height)
+            
+            self.viewController.view.frame = viewFrame
+            }, completion: { finished in
+                debugPrint("Finished");
+        })
+        
+        pluginResult = CDVPluginResult(
+            status: CDVCommandStatus_OK
+        )
+        
+        self.commandDelegate!.sendPluginResult(
+            pluginResult,
+            callbackId: command.callbackId
+        )
+    }
 }
--- www/northPluginDeviceTest.js
+++ www/northPluginDeviceTest.js
@@ -5,9 +5,17 @@ exports.coolMethod = function(arg0, success, error) {
 };
 
 /*
- arg0 = device id, can be: iPhone4, iPhoneSE, iPhone6, iPhone6Plus
+ arg0 = Device id, can be: iPhone4, iPhoneSE, iPhone6, iPhone6Plus
  */
 
 exports.setDevice = function(arg0, success, error) {
   exec(success, error, "northPluginDeviceTest", "setDevice", [arg0]);
+};
+
+/*
+ arg0 = Side to anchor to, can be: left or right
+ */
+
+exports.anchor = function(arg0, success, error) {
+  exec(success, error, "northPluginDeviceTest", "anchor", [arg0]);
 };
